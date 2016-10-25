fc9d9e47cd3d9ff8555094ca3f8f41a342412359
Frédéric Rodrigo <fred.rodrigo@gmail.com>
2016-10-25 19:13:28 +0200

    Support grape translation

--- config/initializers/i18n.rb
+++ config/initializers/i18n.rb
@@ -1,3 +1,4 @@
-I18n.load_path = Dir['./config/locales/*.yml']
+I18n.load_path += Dir['./config/locales/*.yml']
+I18n.load_path += Dir['lib/grape/locale/*.yml']
 I18n::Backend::Simple.include(I18n::Backend::Fallbacks)
 I18n.enforce_available_locales = false
