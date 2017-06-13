802730f870d48ab84de345967a36e47c7a9a3ef8
Frédéric Rodrigo <fred.rodrigo@gmail.com>
2017-06-13 16:58:37 +0200

    Do no output doc version in swagger spec

--- api/api_v01.rb
+++ api/api_v01.rb
@@ -42,6 +42,7 @@ module Api
         'application/json; charset=UTF-8',
         'application/xml',
       ],
+      doc_version: nil,
       info: {
         title: ::OptimizerWrapper::config[:product_title],
         contact_email: ::OptimizerWrapper::config[:product_contact_email],
