6265bc1cf85b2e349cedc7d6957e4a74c7b5adc4
FloMapo <florian.paviot@mapotempo.com>
2017-08-17 11:11:08 +0200

    Limit size of cached value

--- lib/cache_manager.rb
+++ lib/cache_manager.rb
@@ -13,7 +13,8 @@ class CacheManager
   end
 
   def write(name, value, options = nil)
-    @cache.write(name, value, options)
+    # Limit string size to 10kb to avoid cache errors in Redis
+    @cache.write(name, value, options) if value.to_s.bytesize < 10.kilobytes
   rescue StandardError => error
     Api::Root.logger.warn("Got error #{error} attempting to write cache #{name}.")
     return nil
