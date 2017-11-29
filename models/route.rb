683a5e72ee006951d3d1b6318c18f7c649123532
braktar <gwenael@mapotempo.com>
2017-11-29 18:13:27 +0100

    route class

---
+++ models/route.rb
@@ -0,0 +1,25 @@
+# Copyright © Mapotempo, 2017
+#
+# This file is part of Mapotempo.
+#
+# Mapotempo is free software. You can redistribute it and/or
+# modify since you respect the terms of the GNU Affero General
+# Public License as published by the Free Software Foundation,
+# either version 3 of the License, or (at your option) any later version.
+#
+# Mapotempo is distributed in the hope that it will be useful, but WITHOUT
+# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+# or FITNESS FOR A PARTICULAR PURPOSE.  See the Licenses for more details.
+#
+# You should have received a copy of the GNU Affero General Public License
+# along with Mapotempo. If not, see:
+# <http://www.gnu.org/licenses/agpl.html>
+#
+require './models/base'
+
+module Models
+  class Route < Base
+    field :mission_ids, default: []
+    belongs_to :vehicle, class_name: 'Models::Vehicle'
+  end
+end
