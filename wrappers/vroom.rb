811a522c0a9eca7306cebd5240138b94aba48009
braktar <gwenael@mapotempo.com>
2018-02-12 16:53:24 +0100

    Update to actual Vroom/master

--- wrappers/vroom.rb
+++ wrappers/vroom.rb
@@ -75,55 +75,33 @@ module Wrappers
         matrix_indices +
         (!vehicle_loop && vehicle_have_end ? [points[vehicle.end_point_id].matrix_index] : [])
 
-      matrix = vehicle.matrix_blend(vrp.matrices.find{ |matrix| matrix.id == vehicle.matrix_id }, matrix_indices, [:time, :distance], {cost_time_multiplier: vehicle.cost_time_multiplier, cost_distance_multiplier: vehicle.cost_distance_multiplier})
-
-      if vrp.preprocessing_prefer_short_segment
-        matrix = matrix.collect{ |a| a.collect{ |b| (b + 20 * Math.sqrt(b)).round } }
-      end
-
-      result = run_vroom(vehicle_have_start, vehicle_have_end, vehicle_loop, matrix) { |avancement, total|
+      result = run_vroom(vrp.vehicles, vrp.services, points, vrp.matrices, [:time, :distance], vrp.preprocessing_prefer_short_segment) { |avancement, total|
         block.call(self, avancement, total, nil, nil) if block
       }
       return if !result
-
-      tour = result['routes'][0]['steps'].collect{ |step| step['job'] }
+      tour = result['routes'][0]['steps'].collect{ |step| step['job'] }.compact
       puts tour.inspect
 
-      if vehicle_loop
-        index = tour.index(0)
-        tour = tour.rotate(index)
-      end
-
-      if vehicle_loop || vehicle_have_start
-        tour = tour[1..-1]
-      else
-        tour = tour.collect{ |i| i + 1 }
-      end
-
-      if vehicle_loop || vehicle_have_end
-        tour = tour[0..-2]
-      end
-
-      cost = (result['solution']['cost'] / 100) + vehicle.cost_fixed
+      cost = (result['summary']['cost']) + vehicle.cost_fixed
       block.call(self, 1, 1, cost, nil) if block
       previous = vehicle_have_start ? vehicle.start_point.matrix_index : nil
       activities = ([vehicle_have_start ? {
-          point_id: vehicle.start_point.id,
-          detail: vehicle.start_point.location ? {
-            lat: vehicle.start_point.location.lat,
-            lon: vehicle.start_point.location.lon
-          } : nil
-        }.delete_if{ |k,v| !v } : nil] +
-        tour.collect{ |i|
-          point_index = vrp.services[i - 1].activity.point[:matrix_index]
-          point = vrp.points[point_index]
-          service = vrp.services[i - 1]
-          current_activity = {
-            service_id: service.id,
-            point_id: point.id,
-            travel_time: (previous && point_index && vrp.matrices[0][:time] ? vrp.matrices[0][:time][previous][point_index] : 0),
-            travel_distance: (previous && point_index && vrp.matrices[0][:distance] ? vrp.matrices[0][:distance][previous][point_index] : 0),
-            detail: build_detail(service, service.activity, point, nil)
+        point_id: vehicle.start_point.id,
+        detail: vehicle.start_point.location ? {
+          lat: vehicle.start_point.location.lat,
+          lon: vehicle.start_point.location.lon
+        } : nil
+      }.delete_if{ |k,v| !v } : nil] +
+      tour.collect{ |i|
+        point_index = vrp.services[i].activity.point[:matrix_index]
+        point = vrp.points[point_index]
+        service = vrp.services[i]
+        current_activity = {
+          service_id: service.id,
+          point_id: point.id,
+          travel_time: (previous && point_index && vrp.matrices[0][:time] ? vrp.matrices[0][:time][previous][point_index] : 0),
+          travel_distance: (previous && point_index && vrp.matrices[0][:distance] ? vrp.matrices[0][:distance][previous][point_index] : 0),
+          detail: build_detail(service, service.activity, point, nil)
 #          travel_distance 0,
 #          travel_start_time 0,
 #          waiting_duration 0,
@@ -131,30 +109,30 @@ module Wrappers
 #          duration 0,
 #          pickup_shipments_id [:id0:],
 #          delivery_shipments_id [:id0:]
-          }
-          previous = point_index
-          current_activity
-        } +
-        [vehicle_have_end ? {
-          point_id: vehicle.end_point.id,
-          detail: vehicle.end_point.location ? {
-            lat: vehicle.end_point.location.lat,
-            lon: vehicle.end_point.location.lon
-          } : nil
-        }.delete_if{ |k,v| !v } : nil]).compact
+        }
+        previous = point_index
+        current_activity
+      }.compact +
+      [vehicle_have_end ? {
+        point_id: vehicle.end_point.id,
+        detail: vehicle.end_point.location ? {
+          lat: vehicle.end_point.location.lat,
+          lon: vehicle.end_point.location.lon
+        } : nil
+      }.delete_if{ |k,v| !v } : nil]).compact
 
       rests = vehicle.rests
       if vehicle.timewindow && vehicle.timewindow.start
         rests.sort_by!{ |rest| rest.timewindows[0].end ? -rest.timewindows[0].end : -2**31 }.each{ |rest|
-          time = vehicle.timewindow.start + vrp.services[tour[0]-1].activity.duration
+          time = vehicle.timewindow.start + vrp.services[tour[0]].activity.duration
           i = pos_rest = 0
           if vehicle_have_start
-            time += vrp.matrices.find{ |matrix| matrix.id == vehicle.matrix_id }.time[vehicle.start_point.matrix_index][vrp.services[tour[0]-1].activity.point.matrix_index]
+            time += vrp.matrices.find{ |matrix| matrix.id == vehicle.matrix_id }.time[vehicle.start_point.matrix_index][vrp.services[tour[0]].activity.point.matrix_index]
             pos_rest += 1
           end
           if !rest.timewindows[0].end || time < rest.timewindows[0].end
             pos_rest += 1
-            while i < tour.size - 1 && (!rest.timewindows[0].end || (time += vrp.matrices.find{ |matrix| matrix.id == vehicle.matrix_id }.time[vrp.services[tour[i]-1].activity.point.matrix_index][vrp.services[tour[i+1]-1].activity.point.matrix_index] + vrp.services[tour[i+1]-1].activity.duration) < rest.timewindows[0].end) do
+            while i < tour.size - 1 && (!rest.timewindows[0].end || (time += vrp.matrices.find{ |matrix| matrix.id == vehicle.matrix_id }.time[vrp.services[tour[i]].activity.point.matrix_index][vrp.services[tour[i+1]].activity.point.matrix_index] + vrp.services[tour[i+1]].activity.duration) < rest.timewindows[0].end) do
               i += 1
             end
             pos_rest += i
@@ -195,21 +173,34 @@ module Wrappers
       }
     end
 
-    def run_vroom(have_start, have_end, is_loop, matrix)
+    def run_vroom(vehicles, services, points, matrices, dimensions, prefer_short)
       input = Tempfile.new('optimize-vroom-input', tmpdir=@tmp_dir)
-      input.write("NAME: vroom\n")
-      input.write("TYPE: ATSP\n")
-      if !is_loop
-        input.write("START: #{0}\n") if have_start
-        input.write("END: #{matrix.size - 1}\n") if have_end
-      end
-      input.write("DIMENSION: #{matrix.size}\n")
-      input.write("EDGE_WEIGHT_TYPE: EXPLICIT\n")
-      input.write("EDGE_WEIGHT_FORMAT: FULL_MATRIX\n")
-      input.write("EDGE_WEIGHT_SECTION\n")
-      input.write(matrix.collect{ |a| a.collect{ |f| (f * 100).to_i }.join(" ") }.join("\n"))
-      input.write("\n")
-      input.write("EOF\n")
+      problem = { vehicles:[], jobs:[], matrix:[] }
+      vehicle = vehicles.first
+      problem[:vehicles] << {
+        id: 0,
+        start_index: vehicle.start_point_id ? services.size : nil,
+        end_index: vehicle.end_point_id ? services.size + (vehicle.start_point_id ? 1 : 0) : nil
+      }.delete_if{ |k, v| v.nil? }
+      matrix_indices = []
+      problem[:jobs] = services.collect.with_index{ |service, index|
+        matrix_indices << points[service.activity.point_id].matrix_index
+        [{
+          id: index,
+          location_index: index
+        }]
+      }.flatten
+
+      matrix = matrices.find{ |current_matrix| current_matrix.id == vehicle.matrix_id }
+
+      matrix_indices << points[vehicle.start_point_id].matrix_index if vehicle.start_point_id
+      matrix_indices << points[vehicle.end_point_id].matrix_index if vehicle.end_point_id
+
+      agglomerate_matrix = vehicle.matrix_blend(matrix, matrix_indices, [:time, :distance], {cost_time_multiplier: vehicle.cost_time_multiplier, cost_distance_multiplier: vehicle.cost_distance_multiplier})
+      agglomerate_matrix.collect!{ |a| a.collect{ |b| ((100 * b + (prefer_short ? 20 * Math.sqrt(b) : 0))/100 + 0.5) .to_i } }
+
+      problem[:matrix] = agglomerate_matrix
+      input.write(problem.to_json)
       input.close
 
       output = Tempfile.new('optimize-vroom-output', tmpdir=@tmp_dir)
