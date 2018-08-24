daf76a5f890e513e72d416a292bc6ee65674bfb5
Braktar <rgwenael@gmail.com>
2018-08-24 09:54:39 +0200

    No overlaping orders

--- wrappers/ortools.rb
+++ wrappers/ortools.rb
@@ -52,7 +52,12 @@ module Wrappers
     end
 
   def solve(vrp, job, thread_proc = nil, &block)
-    vrp.relations.select{ |relation| relation.type == 'order' }.each{ |relation|
+    order_relations = vrp.relations.select{ |relation| relation.type == 'order' }
+    already_begin = order_relations.collect{ |relation| relation.linked_ids[0..-2] }.flatten
+    duplicated_begins = already_begin.uniq.select{ |linked_id| already_begin.select{ |link| link == linked_id }.size > 1 }
+    already_end = order_relations.collect{ |relation| relation.linked_ids[1..-1] }.flatten
+    duplicated_ends = already_end.uniq.select{ |linked_id| already_end.select{ |link| link == linked_id }.size > 1 }
+    order_relations.select{ |relation| (relation.linked_ids[0..-2] & duplicated_begins).size == 0 && (relation.linked_ids[1..-1] & duplicated_ends).size == 0 }.each{ |relation|
       order_route = {
         id: 'automatic_route_order' + (vrp.vehicles.size == 1 ? vrp.vehicles.first.id : vrp.relations.find{ |relation| relation.type == 'order' }[:id]),
         vehicle: vrp.vehicles.size == 1 ? vrp.vehicles.first : nil,
