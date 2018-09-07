b0a0837fb6e0197f463a27158c112f08c6a8dccf
Braktar <rgwenael@gmail.com>
2018-09-07 10:10:38 +0200

    symbol to string : protobuf 3.3

--- wrappers/ortools.rb
+++ wrappers/ortools.rb
@@ -295,7 +295,7 @@ module Wrappers
           end_index: vehicle.end_point ? points[vehicle.end_point_id].matrix_index : -1,
           duration: vehicle.duration ? vehicle.duration : -1,
           distance: vehicle.distance ? vehicle.distance : -1,
-          shift_preference: (vehicle.force_start ? :force_start : vehicle.shift_preference),
+          shift_preference: (vehicle.force_start ? 'force_start' : vehicle.shift_preference.to_s),
           day_index: vehicle.global_day_index ? vehicle.global_day_index : -1,
           max_ride_time: vehicle.maximum_ride_time || 0,
           max_ride_distance: vehicle.maximum_ride_distance || 0,
