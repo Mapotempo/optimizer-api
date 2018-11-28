4892aeea7775c1aaf9e6efaa0161bc20c6fb52f2
Alexis Lahouze <alexis@lahouze.org>
2018-11-28 12:07:24 +0100

    Add variable to be able to seperate Redis host for Resque.

--- config/initializers/resque.rb
+++ config/initializers/resque.rb
@@ -2,5 +2,5 @@ require 'resque'
 require 'resque-status'
 
 Resque.inline = ENV['APP_ENV'] == 'test'
-Resque.redis = ENV['REDIS_HOST'] || 'localhost'
+Resque.redis = ENV['REDIS_RESQUE_HOST'] || ENV['REDIS_HOST'] || 'localhost'
 Resque::Plugins::Status::Hash.expire_in = (30 * 24 * 60 * 60) # In seconds, a too small value remove working or queuing jobs
