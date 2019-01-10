9683e22b55ca89a702d6b524fb657e583f734276
Braktar <gwenael@mapotempo.com>
2019-01-10 11:36:26 +0100

    Fix wrapper pick < deli

--- wrappers/wrapper.rb
+++ wrappers/wrapper.rb
@@ -112,8 +112,8 @@ module Wrappers
     def assert_no_pickup_timewindows_after_delivery_timewindows(vrp)
       vrp.shipments.empty? || vrp.shipments.none? { |shipment|
         first_open = shipment.pickup.timewindows.sort_by{ |s| s[:start]}.first
-        last_close = shipment.delivery.timewindows.sort_by{ |s| s[:start]}.last
-        (first_open ? first_open.start : 0) + 86400 * (first_open && first_open.day_index ? first_open.day_index : 0) > (last_close ? last_close.start : 0) + 86400 * (last_close && last_close.day_index ? last_close.day_index : 0)
+        last_close = shipment.delivery.timewindows.sort_by{ |s| s[:end]}.last
+        first_open && last_close && (first_open ? first_open.start : 0) + 86400 * (first_open && first_open.day_index ? first_open.day_index : 0) > (last_close ? last_close.start : 0) + 86400 * (last_close && last_close.day_index ? last_close.day_index : 0)
       }
     end
 
