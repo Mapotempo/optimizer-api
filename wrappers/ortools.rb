c3a31a437e830d3ca84ccb0172860bc0bee1f2c6
fonsecadeline <adeline.fonseca@hotmail.com>
2019-01-22 16:27:20 +0100

    evaluate only does not need neighbourhood

--- wrappers/ortools.rb
+++ wrappers/ortools.rb
@@ -736,7 +736,7 @@ module Wrappers
         "#{@exec_ortools} ",
         (vrp.resolution_duration || @optimize_time) && '-time_limit_in_ms ' + (vrp.resolution_duration || @optimize_time).to_s,
         vrp.preprocessing_prefer_short_segment ? '-nearby' : nil,
-        (vrp.resolution_evaluate_only ? '-neighbourhood 0' : (vrp.preprocessing_neighbourhood_size ? "-neighbourhood #{vrp.preprocessing_neighbourhood_size}" : nil)),
+        (vrp.resolution_evaluate_only ? nil : (vrp.preprocessing_neighbourhood_size ? "-neighbourhood #{vrp.preprocessing_neighbourhood_size}" : nil)),
         (vrp.resolution_iterations_without_improvment || @iterations_without_improvment) && '-no_solution_improvement_limit ' + (vrp.resolution_iterations_without_improvment || @iterations_without_improvment).to_s,
         (vrp.resolution_minimum_duration || vrp.resolution_initial_time_out) && '-minimum_duration ' + (vrp.resolution_minimum_duration || vrp.resolution_initial_time_out).to_s,
         (vrp.resolution_time_out_multiplier || @time_out_multiplier) && '-time_out_multiplier ' + (vrp.resolution_time_out_multiplier || @time_out_multiplier).to_s,
