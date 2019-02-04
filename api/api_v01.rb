1cc84a8d738f3c9ed85ec757b558ac8743503770
fonsecadeline <adeline.fonseca@hotmail.com>
2019-02-04 10:46:43 +0100

    route doc

--- api/api_v01.rb
+++ api/api_v01.rb
@@ -388,6 +388,12 @@ __pickup__ and __delivery__ are build following the __activity__ model
     }
   }]
 ```
+### <a name="routes"></a>**Routes**
+Routes structure allows you to provide an initial solution. You can provide routes when calling ORtools solver or periodic heuristic.
+* When using ORtools solver, this route is used as initial solution. That is, we do not use any heuristic to find the first solution but we use provided one.
+If the route is unfeasible we compute initial solution using a heuristic, as if no route was provided.
+* When using periodic heuristic, we start from the solution corresponding to provided routes. If one of the missions is not feasible at its position it will be unassigned.
+
 ### <a name="matrices"></a>**Matrices**
 Describe the topology of the problem, it represent travel time, distance or value between every points,
 Matrices are not mandatory, if time or distance are not defined the router wrapper will use the points data to build it.
