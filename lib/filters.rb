0ea86691c6ce923067fe360cec0cd7d11362a276
halilsen <halil.sen@mapotempo.com>
2019-03-11 12:24:00 +0100

    calculate_unit_precision function implementation and its test Which calculates a coefficient to do - Models::Unit float->int coversion - and balancing between different unit capacities
    New test file for tests related to Filter class
    + test against Integer Range Error: Too big to convert
    + test to check if run_ortools gets the correct unit.precision_coef's
    
    TODO: Implementation of the function is done but the precision_coef is
    not in use. Need to replace "*1000" multiplications with
    "unit.precision_coef". Warning: overload_multiplier needs to be
    divided by this coeff to keep the problem unchanged.
    (And potentially other cost coefs related to quantity and capacity.)

--- lib/filters.rb
+++ lib/filters.rb
@@ -21,7 +21,146 @@ module Filters
   def self.filter(services_vrps)
     services_vrps = merge_timewindows(services_vrps)
 
+    calculate_unit_precision
+
     services_vrps
+  end
+
+  # Calculates a coefficient for each unit so that when multiplied with
+  # this coefficient; each non-integer capacity and quantity (service
+  # and shipment) becomes integer and maximum capacities of different
+  # units becomes balanced (i.e., in the same order of magnitude).
+  #
+  # Needs to be called while preparing the input data of the solvers
+  # which expects integer values for capacitiy and quantity values
+  # (e.g., ortools).
+  def self.calculate_unit_precision
+    return if Models::Unit.all.empty? ||
+              (Models::Capacity.all.empty? && Models::Quantity.all.empty?)
+
+    # Find all quantity and capacity values by unit type to calculate the coefficients
+    all_values_by_units = {}
+
+    # First check the capacities to initialize the hash
+    Models::Capacity.all.each{ |capacity|
+      if all_values_by_units[capacity.unit].nil?
+        all_values_by_units[capacity.unit] = [] #create the array of the unit if first
+      end
+      all_values_by_units[capacity.unit].push(capacity.limit) unless capacity.limit.nil? || capacity.limit.zero?
+    }
+
+    # Then services and shipments
+    Models::Quantity.all.each{ |quantity|
+      if all_values_by_units[quantity.unit].nil? # This unit has no capacity in none of the service_vrps, but we still might turn it into integer
+        all_values_by_units[quantity.unit] = []
+      end
+      all_values_by_units[quantity.unit].push(quantity.value) unless quantity.value.nil? || quantity.value.zero?
+      all_values_by_units[quantity.unit].push(quantity.setup_value) if quantity.unit.counting && !quantity.setup_value.zero?
+    }
+
+    all_values_by_units.reject!{ |_unit, value_array| value_array.empty? }
+
+    float_unit_has_overload_multiplier = false # there is a unit with a float value/limit and a non-zero overload cost
+    # For each unit, find the coefficient to convert all floating point numbers
+    # of this unit (i.e., capacities, quantities of unit_id "x") to integer
+    all_values_by_units.each_pair{ |unit, value_array|
+      unit.precision_coef = smallest_coef_to_make_array_all_integer(value_array).to_r
+      # FIXME: We need to decide what to do if there is an overload_multiplier cost in one unit and it
+      # has a noninteger capacity/quantitiy (i.e., we need to multiply the values of a unit with a precision_coef)
+      # Currently precision_coef is not in use so this souldn't cause a problem.
+      float_unit_has_overload_multiplier = true if !float_unit_has_overload_multiplier &&
+                                                   (unit.precision_coef != Models::Unit.default_attributes[:precision_coef]) &&
+                                                   (Models::Capacity.where(unit: unit).all?{ |cap| cap.overload_multiplier.nil? || cap.overload_multiplier.zero? })
+    }
+
+    if float_unit_has_overload_multiplier ||
+       (Models::Capacity.all.size != Models::Capacity.where(overload_multiplier: 0).size)
+      # FIXME: Even if there is no unit with_overload_cost and with_non_integral_values
+      # we might be still in trouble if there is a non-zero overload cost in some of the
+      # units since normalization and balancing will most certainly modify the precision_coef
+    end
+
+    # Normalise the precision_coef (within the unit) using greatest-common-divisor
+    all_values_by_units.each_pair{ |unit, value_array|
+      integer_value_array = value_array.map{ |value| (value * unit.precision_coef).round }
+      gcd_of_integer_value_array = gcd_of_array(integer_value_array)
+      unit.precision_coef = (unit.precision_coef / gcd_of_integer_value_array).to_r
+    }
+
+    # Skip the following units before balancing:
+    # counting units (the precision_coef turns them into integer, they don't need to be in the balancing)
+    # the units which only has services/shipments without capacity or which only has capacity without any services/shipments
+    all_values_by_units.delete_if{ |unit, value_array|
+      unit.counting ||
+        (value_array.size == Models::Capacity.where(unit: unit).size) ||
+        (value_array.size == Models::Quantity.where(unit: unit).size)
+    }
+
+    return if all_values_by_units.empty?
+
+    # Find the overall max capacity limit and max capacity by unit for balancing
+    max_capacity_by_unit_after_precision_coef = {}
+    all_values_by_units.each_pair{ |unit, value_array|
+      integer_value_array = value_array.map{ |value| (value * unit.precision_coef).round }
+      max_capacity_by_unit_after_precision_coef[unit] = integer_value_array.max
+    }
+    max_overall_capacity_after_prec_coef = max_capacity_by_unit_after_precision_coef.max_by(&:last).last.to_f
+
+    # Lastly balance the coefficient between different units
+    # so that max capacity of each unit is in the same order of magnitude
+    max_capacity_by_unit_after_precision_coef.each_pair{ |unit, max_unit_capacity|
+      next if max_overall_capacity_after_prec_coef == max_unit_capacity
+      unit.precision_coef *= (max_overall_capacity_after_prec_coef.to_f / max_unit_capacity).round
+    }
+
+    return
+  end
+
+  # Returns the smallest coefficient which turns an array of floatig point numbers into integer
+  def self.smallest_coef_to_make_array_all_integer(values)
+    coefficient = 1
+    values.each{ |value|
+      coef = smallest_coef_to_make_value_integer(value)
+      # Lowest common multiple of all coefs
+      # That is if array has 0.1 and 0.25 as values
+      # they, respectively, need be multiplies by 10 and 4 to become integer
+      # and a coef of 20 (=lcm(10,4)) turns both numbers to integer
+      coefficient = coefficient.lcm(coef)
+    }
+    coefficient
+  end
+
+  # Returns the smallest coefficient which turns a floatig point number into integer
+  def self.smallest_coef_to_make_value_integer(value)
+    # Find number of decimal places in the value
+    decimal_place_count = decimal_places(value)
+
+    # Find denominator in fraction form. For example,
+    # for 30.25, denominator is 100
+    denominator = 10**decimal_place_count
+
+    # Result is denominator divided by GCD-of-numerator-and-denominator.
+    # For example, for 30.25, result is 100 / GCD(3025,100) = 100/25 = 4
+    # Round should be okay because we calculate the decimal places exactly
+    denominator / denominator.gcd((value * denominator).round)
+  end
+
+  # Returns the number of decimal places in a value
+  def self.decimal_places(value)
+    decimal_count = 0
+    original_value = value
+    until (value - value.round).abs < 1e-10 # Check if integer (it needs to be done like this due to floating point representation)
+        decimal_count += 1
+        value = original_value * 10**decimal_count # value *= 10 propogates the floating point error
+    end
+    decimal_count
+  end
+
+  # Returns the GCD of an array of numbers
+  def self.gcd_of_array(values)
+    result = values[0]
+    values.each{ |value| result = result.gcd(value) }
+    result
   end
 
   def self.merge_timewindows(services_vrps)
