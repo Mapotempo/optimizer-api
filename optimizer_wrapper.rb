33d278101e19152b50a0de323e8fcbd815d1c162
Braktar <gwenael@mapotempo.com>
2019-03-13 11:59:16 +0100

    Refactor Split & EmptyFill managment

--- optimizer_wrapper.rb
+++ optimizer_wrapper.rb
@@ -174,7 +174,7 @@ module OptimizerWrapper
       multi = Interpreters::MultiTrips.new
       multi.expand(service_vrp[:vrp])
     }
-    result = solve(definitive_service_vrps, services_fleets, job, block)
+    result = solve(definitive_service_vrps, job, block) if !definitive_service_vrps.empty?
     result_global = {
       result: ([result] + duplicated_results + split_results).compact
     }
