e9d82c4638aaf189144ad80fbfed6450e1297606
fab-girard <fab.girard@gmail.com>
2019-05-03 09:10:14 +0200

    Improve perf for timewindows merge

--- lib/filters.rb
+++ lib/filters.rb
@@ -25,7 +25,6 @@ module Filters
   end
 
   def self.merge_timewindows(services_vrps)
-
     services_vrps.each{ |service_vrp|
       service_vrp[:vrp].services.each{ |service|
         next if !service.activity
@@ -33,17 +32,18 @@ module Filters
         inter = {}
         new_timewindows = []
         if service.activity.timewindows.size > 1
-          service.activity.timewindows.each.with_index{ |timewindow, index|
+          service.activity.timewindows.each{ |timewindow|
             unified_timewindows[timewindow.id] = {
               start: timewindow.start && ((timewindow.day_index || 0) * 86400 + timewindow.start) || (0 + (timewindow.day_index || 0) * 86400),
-              end: timewindow.end && ((timewindow.day_index || 0) * 86400 + timewindow.end)  || (0 + (1 + (timewindow.day_index || 6)) * 86400)
+              end: timewindow.end && ((timewindow.day_index || 0) * 86400 + timewindow.end) || (0 + (1 + (timewindow.day_index || 6)) * 86400)
             }
             inter[timewindow.id] = []
           }
           unified_timewindows.each{ |key, value|
             unified_timewindows.each{ |s_key, s_value|
               next if key == s_key || s_value.include?(key) || value.include?(s_key)
-              if !((value[:start].to_i..value[:end].to_i).to_a & (s_value[:start].to_i..s_value[:end].to_i).to_a).empty?
+              if value[:start] >= s_value[:start] && value[:start] <= s_value[:end] ||
+                 value[:end] >= s_value[:start] && value[:end] <= s_value[:end]
                 inter[key].each{ |k_value| inter[k_value] << s_key }
                 inter[s_key].each{ |k_value| inter[k_value] << key }
                 inter[key] << s_key
@@ -57,15 +57,15 @@ module Filters
               to_merge_ids = ([key] + value).uniq
               to_merge_ids.each{ |id| inter.delete(id) }
               inter.delete(to_merge_ids)
-              to_merge_tws = service.activity.timewindows.select{ |timewindow| to_merge_ids.include?(timewindow.id)}
+              to_merge_tws = service.activity.timewindows.select{ |timewindow| to_merge_ids.include?(timewindow.id) }
               day_indices = to_merge_tws.collect{ |tw| tw.day_index }
               starts = to_merge_tws.collect{ |tw| tw.start }
               ends = to_merge_tws.collect{ |tw| tw.end }
               earliest_day_index = day_indices.include?(nil) ? nil : day_indices.min
               latest_day_index = day_indices.include?(nil) ? nil : day_indices.max
-              earliest_start = starts.include?(nil)? nil : starts.min
-              latest_end = ends.include?(nil)? nil: ends.max
-              new_timewindows << Models::Timewindow.new(start: earliest_start, end: latest_end, day_index:earliest_day_index)
+              earliest_start = starts.include?(nil) ? nil : starts.min
+              latest_end = ends.include?(nil) ? nil : ends.max
+              new_timewindows << Models::Timewindow.new(start: earliest_start, end: latest_end, day_index: earliest_day_index)
             }
             service.activity.timewindows = new_timewindows
           end
