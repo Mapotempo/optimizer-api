9fddc1f10443ce0afa7a9a277c98c11d3fd911aa
halilsen <halil.sen@mapotempo.com>
2019-06-12 12:00:43 +0200

    Fix vehicle_limit if less than vehicles.size

--- wrappers/ortools.rb
+++ wrappers/ortools.rb
@@ -756,7 +756,7 @@ module Wrappers
         (vrp.resolution_minimum_duration || vrp.resolution_initial_time_out) && '-minimum_duration ' + (vrp.resolution_minimum_duration || vrp.resolution_initial_time_out).to_s,
         (vrp.resolution_time_out_multiplier || @time_out_multiplier) && '-time_out_multiplier ' + (vrp.resolution_time_out_multiplier || @time_out_multiplier).to_s,
         vrp.resolution_init_duration ? "-init_duration #{vrp.resolution_init_duration}" : nil,
-        vrp.resolution_vehicle_limit ? "-vehicle_limit #{vrp.resolution_vehicle_limit}" : nil,
+        vrp.resolution_vehicle_limit && vrp.resolution_vehicle_limit < problem.vehicles.size ? "-vehicle_limit #{vrp.resolution_vehicle_limit}" : nil,
         vrp.resolution_solver_parameter ? "-solver_parameter #{vrp.resolution_solver_parameter}" : nil,
         vrp.preprocessing_first_solution_strategy ? "-solver_parameter #{correspondant[vrp.preprocessing_first_solution_strategy.first]}" : nil,
         vrp.resolution_evaluate_only || vrp.resolution_batch_heuristic ? '-only_first_solution': nil,
