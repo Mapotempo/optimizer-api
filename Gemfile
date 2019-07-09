ab29c6d15c13dc9283998460aeedfceec150102f
Braktar <gwenael@mapotempo.com>
2019-07-09 17:55:33 +0200

    update Gemfile

--- Gemfile
+++ Gemfile
@@ -8,21 +8,22 @@ gem 'puma'
 gem 'thin'
 gem 'rack-cors'
 
-gem 'grape', '<0.19.0' # TODO: Waiting Ruby 2.2
+gem 'grape'
 gem 'grape_logging'
 gem 'grape-entity'
-gem 'grape-swagger', '<0.26.0' # TODO: Waiting Ruby 2.2
-gem 'grape-swagger-entity', '<0.1.6' # TODO: Waiting Ruby 2.2
+gem 'grape-swagger'
+gem 'grape-swagger-entity'
 
 gem 'i18n'
 gem 'rack-contrib'
 gem 'rest-client'
-gem 'activemodel', '<5' # TODO: Waiting Ruby 2.2
+gem 'activemodel'
 gem 'active_hash', github: 'Mapotempo/active_hash'
+gem 'hashie'
 gem 'nokogiri'
-gem 'resque'
-gem 'resque-status'
-gem 'redis', '<4' # TODO: Waiting Ruby 2.2 (dependency from resque)
+gem 'resque', '<2'
+gem 'resque-status', '>0.4'
+gem 'redis', '<4'
 
 gem 'ai4r'
 gem 'sim_annealing'
@@ -37,7 +38,7 @@ group :development, :test do
   gem 'byebug'
   gem 'benchmark-ips'
   # For linting and offline code analysis in vscode
-  gem 'rubocop', '<0.58' # TODO: Waiting Ruby 2.2
+  gem 'rubocop'
   gem 'solargraph'
 
   ## Next gems to use the debuger of vscode directly
@@ -49,7 +50,7 @@ group :development, :test do
 end
 
 group :test do
-  gem 'rack-test', '<0.8' # TODO: Waiting Ruby 2.2
+  gem 'rack-test'
   gem 'minitest'
   gem 'minitest-reporters'
   gem 'minitest-stub_any_instance'
--- api/v01/vrp.rb
+++ api/v01/vrp.rb
@@ -496,9 +496,9 @@ module Api
             begin
               checksum = Digest::MD5.hexdigest Marshal.dump(params)
               if params[:points]
-                APIBase.dump_vrp_cache.write([params[:api_key], params[:vrp] && params[:vrp][:name], checksum].compact.join('_').parameterize(''), {vrp: { points: params[:points], units: params[:units], timewindows: params[:timewindows], capacities: params[:capacities], quantities: params[:quantities], services: params[:services], shipments: params[:shipments], vehicles: params[:vehicles], configuration: params[:vrp][:configuration] } }.to_json)
+                APIBase.dump_vrp_cache.write([params[:api_key], params[:vrp] && params[:vrp][:name], checksum].compact.join('_').parameterize(separator: ''), {vrp: { points: params[:points], units: params[:units], timewindows: params[:timewindows], capacities: params[:capacities], quantities: params[:quantities], services: params[:services], shipments: params[:shipments], vehicles: params[:vehicles], configuration: params[:vrp][:configuration] } }.to_json)
               else
-                APIBase.dump_vrp_cache.write([params[:api_key], params[:vrp] && params[:vrp][:name], checksum].compact.join('_').parameterize(''), {vrp: params[:vrp]}.to_json)
+                APIBase.dump_vrp_cache.write([params[:api_key], params[:vrp] && params[:vrp][:name], checksum].compact.join('_').parameterize(separator: ''), {vrp: params[:vrp]}.to_json)
               end
               vrp = ::Models::Vrp.create({})
               params_limit = APIBase.services(params[:api_key])[:params_limit].merge(OptimizerWrapper.access[params[:api_key]][:params_limit] || {})
