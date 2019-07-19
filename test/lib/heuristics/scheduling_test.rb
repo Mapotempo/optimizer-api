dca517e8b08886e22d469e2c2935c8e842c6b19d
fonsecadeline <adeline.fonseca@hotmail.com>
2019-07-19 15:20:30 +0200

    improve test

--- test/lib/heuristics/scheduling_test.rb
+++ test/lib/heuristics/scheduling_test.rb
@@ -389,7 +389,6 @@ class HeuristicTest < Minitest::Test
   end
 
   def test_day_closed_on_work_day
-    skip 'Requires modifications in clustering : No vehicle found related'
     problem = VRP.lat_lon_scheduling
     problem[:services][0][:visits_number] = 3
     problem[:services][0][:minimum_lapse] = 7
