89ad430d6900745fbfc6415d44686dc167f50da3
halilsen <halil.sen@mapotempo.com>
2019-11-25 13:49:53 +0100

    Fix: log

--- models/concerns/distance_matrix.rb
+++ models/concerns/distance_matrix.rb
@@ -62,7 +62,7 @@ module DistanceMatrix
       uniq_need_matrix = Hash[uniq_need_matrix.collect{ |mode, dimensions, options|
         block.call(nil, i += 1, uniq_need_matrix.size, 'compute matrix', nil, nil, nil) if block && !self.dichotomious_process?
         # set matrix_time and matrix_distance depending of dimensions order
-        log "matrix computation #{matrix_points}x#{matrix_points}"
+        log "matrix computation #{matrix_points.size}x#{matrix_points.size}"
         router_matrices = OptimizerWrapper.router.matrix(OptimizerWrapper.config[:router][:url], mode, dimensions, matrix_points, matrix_points, options)
         m = Models::Matrix.create(
           id: 'm' + (id += 1).to_s,
