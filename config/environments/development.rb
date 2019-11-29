b303fb39b7fa4b6cc3cbd071d909512c367b2a53
fonsecadeline <adeline.fonseca@hotmail.com>
2019-11-29 10:37:49 +0100

    fix parameters to dump

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -67,8 +67,8 @@ module OptimizerWrapper
       url: ENV['ROUTER_URL'] || 'http://localhost:4899/0.1'
     },
     dump: {
-      vrp: ENV['OPTIM_DUMP_VRP'] == 'true' || true,
-      solution: ENV['OPTIM_DUMP_SOLUTION'] == 'true' || true
+      vrp: ENV['OPTIM_DUMP_VRP'] ? ENV['OPTIM_DUMP_VRP'] == 'true' : true,
+      solution: ENV['OPTIM_DUMP_SOLUTION'] ? ENV['OPTIM_DUMP_SOLUTION'] == 'true' : true
     },
     debug: {
       output_clusters: ENV['OPTIM_DBG_OUTPUT_CLUSTERS'] == 'true',
