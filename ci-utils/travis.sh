30d8a913d8f9918640c7edd0d1026a39802a7c3e
halilsen <halil.sen@mapotempo.com>
2020-01-30 15:00:34 +0100

    Test: increase log level on travis to info

--- ci-utils/travis.sh
+++ ci-utils/travis.sh
@@ -3,29 +3,31 @@
 docker stack deploy -c ./docker/travis-dc.yml optimizer
 
 TEST_ENV=''
+TEST_LOG_LEVEL='info'
+TEST_COVERAGE='false'
 DOCKER_SERVICE_NAME=optimizer_api
 CONTAINER=${DOCKER_SERVICE_NAME}.1.$(docker service ps -f "name=${DOCKER_SERVICE_NAME}.1" ${DOCKER_SERVICE_NAME} -q --no-trunc | head -n1)
 case "$1" in
   'basis')
-    TEST_ENV='TRAVIS=true COV=false SKIP_DICHO=true SKIP_JSPRIT=true SKIP_REAL_CASES=true SKIP_SCHEDULING=true SKIP_SPLIT_CLUSTERING=true'
+    TEST_ENV="TRAVIS=true COV=${TEST_COVERAGE} LOG_LEVEL=${TEST_LOG_LEVEL} SKIP_DICHO=true SKIP_JSPRIT=true SKIP_REAL_CASES=true SKIP_SCHEDULING=true SKIP_SPLIT_CLUSTERING=true"
     ;;
   'dicho')
-    TEST_ENV='TRAVIS=true COV=false TEST=test/lib/heuristics/dichotomious_test.rb'
+    TEST_ENV="TRAVIS=true COV=${TEST_COVERAGE} LOG_LEVEL=${TEST_LOG_LEVEL} TEST=test/lib/heuristics/dichotomious_test.rb"
     ;;
   'real')
-    TEST_ENV='TRAVIS=true COV=false TEST=test/real_cases_test.rb'
+    TEST_ENV="TRAVIS=true COV=${TEST_COVERAGE} LOG_LEVEL=${TEST_LOG_LEVEL} TEST=test/real_cases_test.rb"
     ;;
   'real_scheduling')
-    TEST_ENV='TRAVIS=true COV=false TEST=test/real_cases_scheduling_test.rb'
+    TEST_ENV="TRAVIS=true COV=${TEST_COVERAGE} LOG_LEVEL=${TEST_LOG_LEVEL} TEST=test/real_cases_scheduling_test.rb"
     ;;
   'real_scheduling_solver')
-    TEST_ENV='TRAVIS=true COV=false TEST=test/real_cases_scheduling_solver_test.rb'
+    TEST_ENV="TRAVIS=true COV=${TEST_COVERAGE} LOG_LEVEL=${TEST_LOG_LEVEL} TEST=test/real_cases_scheduling_solver_test.rb"
     ;;
   'scheduling')
-    TEST_ENV="TRAVIS=true COV=false TEST=test/lib/heuristics/scheduling_*"
+    TEST_ENV="TRAVIS=true COV=${TEST_COVERAGE} LOG_LEVEL=${TEST_LOG_LEVEL} TEST=test/lib/heuristics/scheduling_*"
     ;;
   'split_clustering')
-    TEST_ENV='TRAVIS=true COV=false INTENSIVE_TEST=true TEST=test/lib/interpreters/split_clustering_test.rb'
+    TEST_ENV="TRAVIS=true COV=${TEST_COVERAGE} LOG_LEVEL=${TEST_LOG_LEVEL} INTENSIVE_TEST=true TEST=test/lib/interpreters/split_clustering_test.rb"
     ;;
   *)
     ;;
