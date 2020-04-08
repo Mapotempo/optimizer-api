bb7b68bfbc5ce66b5deae5841b31d540e1e998b5
Alain ANDRÉ <wordsbybird@alain-andre.fr>
2020-04-08 15:19:23 +0200

    CI, Docker : Remove ortools, which comes from optimizer-ortools, manage tags

--- ci-utils/travis.sh
+++ ci-utils/travis.sh
@@ -1,12 +1,12 @@
 #!/usr/bin/env bash
-
-docker stack deploy -c ./docker/travis-dc.yml optimizer
+docker stack deploy -c ./docker/docker-compose.yml optimizer
 
 TEST_ENV=''
 TEST_LOG_LEVEL='info'
 TEST_COVERAGE='false'
 DOCKER_SERVICE_NAME=optimizer_api
 CONTAINER=${DOCKER_SERVICE_NAME}.1.$(docker service ps -f "name=${DOCKER_SERVICE_NAME}.1" ${DOCKER_SERVICE_NAME} -q --no-trunc | head -n1)
+
 case "$1" in
   'basis')
     TEST_ENV="TRAVIS=true COV=${TEST_COVERAGE} LOG_LEVEL=${TEST_LOG_LEVEL} SKIP_DICHO=true SKIP_JSPRIT=true SKIP_REAL_CASES=true SKIP_SCHEDULING=true SKIP_SPLIT_CLUSTERING=true"
@@ -42,9 +42,7 @@ do
   sleep 1
 done
 
-docker exec -i ${CONTAINER} bash -c 'ls -l test/'
 docker exec -i ${CONTAINER} apt update -y > /dev/null
 docker exec -i ${CONTAINER} apt install git -y > /dev/null
-docker exec -i ${CONTAINER} rm /srv/app/.bundle/config
-docker exec -i ${CONTAINER} bundle install
+
 docker exec -i ${CONTAINER} bundle exec rake test ${TEST_ENV}
