69f9baf39d70610509564eb19ec579e72b2b70f5
Braktar <gwenael@mapotempo.com>
2020-06-23 17:25:14 +0200

    Travis PR

---
+++ ci-utils/version.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+
+if [[ $TRAVIS_BRANCH == "master" ]] || [[ $TRAVIS_TAG != "" ]]; then
+  export OPTIMIZER_ORTOOLS_VERSION="v1.3.0";
+else
+  export OPTIMIZER_ORTOOLS_VERSION=${OPTIMIZER_ORTOOLS_VERSION:-Mapotempo};
+fi
