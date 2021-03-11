9c1cd1a16a4e23b5ea8e029830893a6891a2582a
Alain ANDRE <wordsbybird@alain-andre.fr>
2021-03-11 19:28:48 +0100

    Move to Github Actions

---
+++ .github/actions/tests.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+
+CONTAINER=${PROJECT}_api.1.$(docker service ps -f "name=${PROJECT}_api.1" ${PROJECT}_api -q --no-trunc | head -n1)
+docker exec -i ${CONTAINER} bundle exec rake test ${OPTIONS}
