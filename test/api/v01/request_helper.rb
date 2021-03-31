12665ea987d7bc09a8ca0744acdc247cac319c50
fonsecadeline <adeline.fonseca@mapotempo.com>
2021-03-31 09:18:53 +0200

    Avoid loop in failing test

--- test/api/v01/request_helper.rb
+++ test/api/v01/request_helper.rb
@@ -26,6 +26,7 @@ module TestHelper
       get "0.1/vrp/jobs/#{job_id}.json", options
 
       assert_equal 200, last_response.status, last_response.body
+      assert_includes ['queued', 'working', 'completed'], JSON.parse(last_response.body)['job']['status']
 
       puts "Empty response body: #{JSON.parse(last_response.body)}" if JSON.parse(last_response.body).nil? || JSON.parse(last_response.body)['job'].nil?
 
@@ -43,6 +44,9 @@ module TestHelper
       get "0.1/vrp/jobs/#{job_id}", options
 
       assert_equal 200, last_response.status, last_response.body
+      unless last_response.body.is_a?(String)
+        assert_includes ['queued', 'working', 'completed'], JSON.parse(last_response.body)['job']['status']
+      end
 
       break if last_response.content_type == 'text/csv;'
 
