f956b20cbde488b84e871c30909d7017ac82bb12
halilsen <halil.sen@mapotempo.com>
2021-04-19 15:00:52 +0200

    CI: add TESTOPTS

--- .github/actions/tests.sh
+++ .github/actions/tests.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
 CONTAINER=${PROJECT}_api.1.$(docker service ps -f "name=${PROJECT}_api.1" ${PROJECT}_api -q --no-trunc | head -n1)
-docker exec -i ${CONTAINER} rake test ${OPTIONS}
+docker exec -i ${CONTAINER} rake test TESTOPTS="${TESTOPTS}" ${OPTIONS}
