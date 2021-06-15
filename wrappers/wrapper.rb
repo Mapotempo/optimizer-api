65e446605d9f7a067aa4538e1bfa10d95a1f3108
fonsecadeline <adeline.fonseca@mapotempo.com>
2021-06-15 09:11:50 +0200

    Do not simplify rests with exclusion_cost

--- wrappers/wrapper.rb
+++ wrappers/wrapper.rb
@@ -952,7 +952,8 @@ module Wrappers
           # TODO: (r.timewindows&.size.to_i > 1) having multiple TWs for pauses is possible but even harder.
           # If necessary, this implementation can be extended to handle this case by re-optimizing with or-tools
           # in a generic way
-          next if vehicle.rests.size > 1 || vehicle.rests.any?{ |r| r.timewindows&.size.to_i > 1 }
+          next if vehicle.rests.size > 1 ||
+                  vehicle.rests.any?{ |r| r.timewindows&.size.to_i > 1 || r.exclusion_cost.to_f.positive? }
 
           # If there is a service longer than the timewindow of the rest then we cannot be sure to
           # insert the pause without inducing unnecessary idle time
