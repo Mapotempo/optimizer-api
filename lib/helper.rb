bb74f94d85a0e8ce2651dd57385f5b8a9d2ddcf4
halilsen <halil.sen@mapotempo.com>
2021-07-19 12:12:44 +0200

    Helper deep_copy function
    TODO: after validation this logic can be moved to base.rb
    TODO: a "shallow_copy" version can be implemented by "prioritizing"
          id-methods instead of non-id methods during copy, and by reverting
    			the override logic via options[:key_id]

--- lib/helper.rb
+++ lib/helper.rb
@@ -63,6 +63,82 @@ module Helper
     111321 * Math.sqrt(delta_lat**2 + delta_lon**2) # 111321 is the length of a degree (of lon and lat) in meters
   end
 
+  def self.deep_copy(original, override: {}, shallow_copy: [])
+    # TODO: after testing move the logic to base.rb as a clone function (initialize_copy)
+
+    # To override keys (and sub-keys) with values instead of using the originals
+    # (i.e., original[:key1], original[:other][:key2] etc) do
+    #                 override: { key1: value1, key2: value2 }
+    # To prevent deep copy and use the original objects and sub-objects
+    # (i.e., original[:key1], original[other][:key2]) do
+    #                 shallow_copy: [:key1, :key2 ]
+
+    # Assigning nil to a key in the override hash, skips the key and key_id of the objects
+
+    # WARNING: custom fields will be missing from the objects! because if a key/method doesn't exist
+    # in the Models::Class definition, it cannot be duplicated
+
+    case original
+    when Array # has_many
+      original.collect{ |val| deep_copy(val, override: override, shallow_copy: shallow_copy) }
+    when Models::Base # belongs_to
+      raise 'Keys cannot be both overridden and shallow copied' if (override.keys & shallow_copy).any?
+
+      # if an option doesn't exist for the current object level, pass it to lower levels
+      unused_override = override.select{ |key, _value|
+        [
+          key, "#{key}_id", "#{key[0..-2]}_ids", "#{key[0..-4]}y_ids", key[0..-4], "#{key[0..-6]}ies", "#{key[0..-5]}s"
+        ].none?{ |k| original.class.method_defined?(k.to_sym) }
+      }
+
+      unused_shallow_copy = shallow_copy.select{ |key|
+        [
+          key, "#{key}_id", "#{key[0..-2]}_ids", "#{key[0..-4]}y_ids", key[0..-4], "#{key[0..-6]}ies", "#{key[0..-5]}s"
+        ].none?{ |k| original.class.method_defined?(k.to_sym) }
+      }
+
+      # if a non-"id" version of the key exists, then prefer the hash of the non-id method (i.e., skip x_id(s))
+      # so that the objects are generated from scratch instead of re-used.
+      # Unless the key or key_id is marked as shallow_copy (then use the object) or the key or key_id is given in override.
+      keys = original.attributes.keys.flat_map{ |key|
+        [
+          key[0..-4].to_sym, "#{key[0..-6]}ies".to_sym, "#{key[0..-5]}s".to_sym, key
+        ].find{ |k| original.class.method_defined?(k) }
+      }.uniq - [:id] # cannot duplicate the object with the same id
+
+      # To reuse the same sub-members, the key needs to be given in the shallow_copy
+      # (which forces the duplication to use the original key object)
+      keys.map!{ |key|
+        if override.key?(key)
+          next unless override[key].nil?
+
+          [
+            "#{key}_id".to_sym, "#{key[0..-2]}_ids".to_sym, "#{key[0..-4]}y_ids".to_sym
+          ].find{ |k| original.class.method_defined?(k) && (!override.key?(k) || override[k]) }
+        elsif ["#{key}_id", "#{key[0..-2]}_ids", "#{key[0..-4]}y_ids"].any?{ |k| override[k.to_sym] }
+          next
+        else
+          key
+        end
+      }.compact!
+
+      # if a key is supplied in the override manually as nil, this means removing the key
+      # pass unused_override and unused_shallow_copy to the lower levels only
+      keys |= override.keys.select{ |k| override[k] && original.class.method_defined?(k) }
+      keys |= shallow_copy.select{ |k| original.class.method_defined?(k) }
+
+      # prefer the option if supplied
+      original.class.create(keys.each_with_object({}) { |key, data|
+        data[key] = override[key] ||
+                    (shallow_copy.include?(key) ? original.send(key) : deep_copy(original.send(key),
+                                                                                 override: unused_override,
+                                                                                 shallow_copy: unused_shallow_copy))
+      })
+    else
+      original.dup
+    end
+  end
+
   def self.merge_results(results, merge_unassigned = true)
     results.flatten!
     results.compact!
