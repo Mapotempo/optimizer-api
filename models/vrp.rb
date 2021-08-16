fe108c1ecfc5993f1d190e2b84713989c80e65c8
fonsecadeline <adeline.fonseca@mapotempo.com>
2021-08-16 15:50:40 +0200

    Update models/vrp.rb
    Co-authored-by: Gwénaël Rault <rgwenael@gmail.com>

--- models/vrp.rb
+++ models/vrp.rb
@@ -470,7 +470,7 @@ module Models
       convert_possible_dates_into_indices(element, hash, start_index)
 
       element[:first_possible_days] = element[:first_possible_day_indices].to_a.slice(0..(element[:visits_number] || 1) - 1)
-      element[:last_possible_days] = element[:last_possible_day_indices].to_a.slice(0..element[:visits_number] || 1 - 1)
+      element[:last_possible_days] = element[:last_possible_day_indices].to_a.slice(0..(element[:visits_number] || 1) - 1)
 
       %i[first_possible_day_indices first_possible_dates last_possible_day_indices last_possible_dates].each{ |k|
         element.delete(k)
