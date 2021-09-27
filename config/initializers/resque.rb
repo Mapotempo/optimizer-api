074735ca9f51575807f9aae6d328a332eacc34ac
fab-girard <fab.girard@gmail.com>
2021-09-27 21:28:43 +0200

    Data in redis has to expire and change timeout

--- config/initializers/resque.rb
+++ config/initializers/resque.rb
@@ -6,4 +6,4 @@ Resque.redis = Redis.new(
   host: ENV['REDIS_RESQUE_HOST'] || ENV['REDIS_HOST'] || 'localhost',
   timeout: 300.0
 )
-Resque::Plugins::Status::Hash.expire_in = (30 * 24 * 60 * 60) # In seconds, a too small value remove working or queuing jobs
+Resque::Plugins::Status::Hash.expire_in = 7.days # In seconds, a too small value remove working or queuing jobs
