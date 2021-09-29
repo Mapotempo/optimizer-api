74b4ba438020cb7825b3524f298342f65efe0848
halilsen <halil.sen@mapotempo.com>
2021-09-29 11:16:58 +0200

    Set read and write timeout values manually

--- config/initializers/resque.rb
+++ config/initializers/resque.rb
@@ -2,5 +2,9 @@ require 'resque'
 require 'resque-status'
 
 Resque.inline = ENV['APP_ENV'] == 'test'
-Resque.redis = ENV['REDIS_RESQUE_HOST'] || ENV['REDIS_HOST'] || 'localhost'
+Resque.redis = Redis.new(
+  host: ENV['REDIS_RESQUE_HOST'] || ENV['REDIS_HOST'] || 'localhost',
+  read_timeout: 180.0,
+  write_timeout: 180.0
+)
 Resque::Plugins::Status::Hash.expire_in = (30 * 24 * 60 * 60) # In seconds, a too small value remove working or queuing jobs
