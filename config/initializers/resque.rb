ba7ccb2a5dc54c8d49d771caf7320136c3cc63e1
halilsen <halil.sen@mapotempo.com>
2021-09-30 11:43:19 +0200

    Increase all Redis timeouts

--- config/initializers/resque.rb
+++ config/initializers/resque.rb
@@ -4,7 +4,6 @@ require 'resque-status'
 Resque.inline = ENV['APP_ENV'] == 'test'
 Resque.redis = Redis.new(
   host: ENV['REDIS_RESQUE_HOST'] || ENV['REDIS_HOST'] || 'localhost',
-  read_timeout: 180.0,
-  write_timeout: 180.0
+  timeout: 300.0
 )
 Resque::Plugins::Status::Hash.expire_in = (30 * 24 * 60 * 60) # In seconds, a too small value remove working or queuing jobs
