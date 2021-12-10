0fa91464791f05e6759ce91f446682709796aff3
halilsen <halil.sen@mapotempo.com>
2021-12-10 15:37:41 +0100

    Use logger

--- util/job_manager.rb
+++ util/job_manager.rb
@@ -38,7 +38,8 @@ module OptimizerWrapper
       Job.current_job_id = self.uuid
       tick('Starting job') # Important to kill job before any code
 
-      puts "#{self.uuid} - Starting job... " + options['checksum']
+      log "Starting job... #{options['checksum']}"
+
       job_started_at = Time.now
       Raven.tags_context(vrp_checksum: options['checksum'])
       key_print = options['api_key'].rpartition('-')[0]
@@ -47,7 +48,7 @@ module OptimizerWrapper
       Raven.user_context(api_key: options['api_key']) # Filtered in sentry if user_context
 
       services_vrps = Marshal.load(Base64.decode64(self.options['services_vrps'])) # Get the vrp
-      puts "#{self.uuid} - Vrp size: #{services_vrps.size} Key print: #{key_print} Names: #{services_vrps.map{ |sv| sv[:vrp].name }}"
+      log "Vrp size: #{services_vrps.size} Key print: #{key_print} Names: #{services_vrps.map{ |sv| sv[:vrp].name }}"
       Raven.extra_context(vrp_names: services_vrps.map{ |sv| sv[:vrp].name })
       self.options['services_vrps'] = nil # The worker is about to launch the optimization, we can delete the vrp from the job
 
@@ -80,8 +81,8 @@ module OptimizerWrapper
           Result.set(self.uuid, p)
         end
       }
-      puts "#{self.uuid} - Ending job... " + options['checksum']
-      puts "#{self.uuid} - Elapsed time: #{(Time.now - job_started_at).round(2)}s Vrp size: #{services_vrps.size} Key print: #{key_print} Names: #{services_vrps.map{ |sv| sv[:vrp].name }}"
+      log "Ending job... #{options['checksum']}"
+      log "Elapsed time: #{(Time.now - job_started_at).round(2)}s Vrp size: #{services_vrps.size} Key print: #{key_print} Names: #{services_vrps.map{ |sv| sv[:vrp].name }}"
 
       # Add values related to the current solve status
       p = Result.get(self.uuid) || {}
