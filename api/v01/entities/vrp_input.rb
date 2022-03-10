209f1068bd48938e7737a78476fde59fcbc5c7bc
halilsen <halil.sen@mapotempo.com>
2022-03-10 11:49:23 +0100

    activity or activities cannot be empty

--- api/v01/entities/vrp_input.rb
+++ api/v01/entities/vrp_input.rb
@@ -355,10 +355,10 @@ module VrpMissions
                       desc: 'Particular abilities required by a vehicle to perform this service. Not available with periodic heuristic.')
 
     optional(:type, type: Symbol, desc: '`service`, `pickup` or `delivery`. Only service type is available with periodic heuristic.')
-    optional(:activity, type: Hash, desc: 'Details of the activity performed to accomplish the current service') do
+    optional(:activity, type: Hash, allow_blank: false, desc: 'Details of the activity performed to accomplish the current service') do
       use :vrp_request_activity
     end
-    optional(:activities, type: Array, desc: 'Define other possible activities for the service. This allows to assign different timewindows and/or points to a single service.') do
+    optional(:activities, type: Array, allow_blank: false, desc: 'Define other possible activities for the service. This allows to assign different timewindows and/or points to a single service.') do
       use :vrp_request_activity
     end
     exactly_one_of :activity, :activities
