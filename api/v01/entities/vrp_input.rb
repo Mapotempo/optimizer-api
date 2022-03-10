d190b03d8fc973c5add7db30182a80deca1d8867
halilsen <halil.sen@mapotempo.com>
2022-03-10 11:35:09 +0100

    Either activity or activities is required

--- api/v01/entities/vrp_input.rb
+++ api/v01/entities/vrp_input.rb
@@ -361,7 +361,7 @@ module VrpMissions
     optional(:activities, type: Array, desc: 'Define other possible activities for the service. This allows to assign different timewindows and/or points to a single service.') do
       use :vrp_request_activity
     end
-    mutually_exclusive :activity, :activities
+    exactly_one_of :activity, :activities
     optional(:quantity_ids, type: String, documentation: { hidden: true }, desc: 'Quantities to consider, CSV front only')
     optional(:quantities, type: Array, desc: 'Define the entities which are taken or dropped. The maximum precision supported is 1e-3.') do
       use :vrp_request_quantity
