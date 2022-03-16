c267abbf6cbf42ee545914d6714fe25f066948f6
halilsen <halil.sen@mapotempo.com>
2022-03-16 18:12:37 +0100

    Expose dicho_inclusion_rate internally

--- api/v01/entities/vrp_input.rb
+++ api/v01/entities/vrp_input.rb
@@ -185,6 +185,7 @@ module VrpConfiguration
     optional(:variation_ratio, type: Integer, desc: 'Value of the ratio that will change the matrice')
     optional(:repetition, type: Integer, documentation: { hidden: true }, desc: 'Number of times the optimization process is going to be repeated. Only the best solution is returned.')
     optional(:dicho_algorithm_service_limit, type: Integer, documentation: { hidden: true }, desc: 'Minimum number of services required to allow a call to heuristic dichotomious_approach')
+    optional(:dicho_inclusion_rate, type: Float, values: ->(v) { v > 0 }, documentation: { hidden: true }, desc: 'Approximate minimum load rate for the solver to consider using a vehicle')
     at_least_one_of :duration, :iterations, :iterations_without_improvment, :stable_iterations, :stable_coefficient, :initial_time_out, :minimum_duration
     mutually_exclusive :initial_time_out, :minimum_duration
     mutually_exclusive :solver, :solver_parameter
