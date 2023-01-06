5d7306ff5b56e96a39dd5f8ecd4b6b894807d5ea
Pierre Graber <pierre.graber@mapotempo.fr>
2023-01-06 18:09:30 +0100

    add some tests for create_service_attributes process() function

--- unconstrained-initialization/knowledge_sources/tests/create_services_attributes_from_problem_test.py
+++ unconstrained-initialization/knowledge_sources/tests/create_services_attributes_from_problem_test.py
@@ -1,12 +1,12 @@
 from unittest.mock import Mock, patch
 import pytest
+import numpy
 import copy
 
 from knowledge_sources.create_services_attributes_from_problem import CreateServicesAttributesFromProblem
 
 problem =  { "services": [
                 {'duration': 20,
-                'priority': 4,
                 'vehicleIndices': [0],
                 'matrixIndex': 2,
                 'id': '470d440f',
@@ -22,7 +22,6 @@ problem =  { "services": [
                 'problemIndex': 0,
                 'alternativeIndex': 0},
                 {'duration': 20,
-                'priority': 4,
                 'vehicleIndices': [0],
                 'matrixIndex': 2,
                 'id': '470d440g',
@@ -39,13 +38,6 @@ problem =  { "services": [
                 'alternativeIndex': 0}
             ]
             }
-
-def test_no_problem():
-    blackboard = Mock(problem = problem)
-
-    knowledge_source = CreateServicesAttributesFromProblem(blackboard)
-
-    assert knowledge_source.verify() == True
 
 def test_verify_missing_TW_on_service():
     blackboard = Mock(problem = copy.deepcopy(problem))
@@ -71,3 +63,50 @@ def test_verify_missing_id_on_service():
 
     with pytest.raises(AttributeError):
         knowledge_source.verify()
+
+def test_no_problem():
+    blackboard = Mock(problem = problem)
+
+    knowledge_source = CreateServicesAttributesFromProblem(blackboard)
+
+    assert knowledge_source.verify() == True
+
+def test_size_of_arrays():
+    blackboard = Mock(problem = problem)
+    knowledge_source = CreateServicesAttributesFromProblem(blackboard)
+    knowledge_source.process()
+
+    assert blackboard.size                   == 2
+    assert blackboard.start_tw.size          == blackboard.size + 1
+    assert blackboard.end_tw.size            == blackboard.size + 1
+    assert blackboard.durations.size         == blackboard.size + 1
+    assert blackboard.setup_durations.size   == blackboard.size + 1
+    assert blackboard.services_volume.size   == blackboard.size + 1
+
+def test_durations_array():
+    blackboard = Mock(problem = problem)
+    knowledge_source = CreateServicesAttributesFromProblem(blackboard)
+    knowledge_source.process()
+
+    assert blackboard.durations.all() ==  numpy.array([ 0., 20., 20.]).all()
+
+def test_setup_durations_array():
+    blackboard = Mock(problem = problem)
+    knowledge_source = CreateServicesAttributesFromProblem(blackboard)
+    knowledge_source.process()
+
+    assert blackboard.setup_durations.all() ==  numpy.array([ 0., 0., 0.]).all()
+
+def test_start_tw_array():
+    blackboard = Mock(problem = problem)
+    knowledge_source = CreateServicesAttributesFromProblem(blackboard)
+    knowledge_source.process()
+
+    assert blackboard.start_tw.all() ==  numpy.array([ 0., 0., 0.]).all()
+
+def test_end_tw_array():
+    blackboard = Mock(problem = problem)
+    knowledge_source = CreateServicesAttributesFromProblem(blackboard)
+    knowledge_source.process()
+
+    assert blackboard.end_tw.all() ==  numpy.array([ -1., -1., -1.]).all()
